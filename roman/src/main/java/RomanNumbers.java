34e12e3eb394979e8d3babfec2918479dbf6979a
gardemanuelsen <garema@student.kristiania.no>
2021-09-06 14:34:10 +0200

    Done 5 tests

--- roman/src/main/java/RomanNumbers.java
+++ roman/src/main/java/RomanNumbers.java
@@ -4,5 +4,18 @@ public class RomanNumbers {
         System.out.println("Hello world");
     }
 
-
+    public static String toRoman(int number) {
+        String result = "";
+        if(number < 4) {
+            for (int i = 0; i < number; i++) {
+                result += "I";
+            }
+        }else if(number == 4) {
+            result += "IV";
+        }
+        else{
+            result += "V";
+        }
+        return result;
+    }
 }
--- roman/src/test/java/RomanTest.java
+++ roman/src/test/java/RomanTest.java
@@ -1,8 +1,39 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RomanTest {
 
-    public static void main(String[] args) {
-
+    @Test
+    void shouldConvert1toI (){
+        assertEquals("I", RomanNumbers.toRoman(1));
     }
+
+    @Test
+    void shouldConvert2toII (){
+        assertEquals("II", RomanNumbers.toRoman(2));
+    }
+
+    @Test
+    void shouldConvert3toIII (){
+        assertEquals("III", RomanNumbers.toRoman(3));
+    }
+
+    @Test
+    void shouldConvert4toIV (){
+        assertEquals("IV", RomanNumbers.toRoman(4));
+    }
+
+    @Test
+    void shouldConvert5toV (){
+        assertEquals("V", RomanNumbers.toRoman(5));
+    }
+
+
+
+
+
+
 
 
 }
